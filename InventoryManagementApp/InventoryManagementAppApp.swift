1ec38f2c85853b1e40e654e0eae6727918919d7e
VipulNita <vipulknita18@gmail.com>
2024-08-22 00:18:10 +0530

    Initial Commit

---
+++ InventoryManagementApp/InventoryManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryManagementAppApp.swift
+//  InventoryManagementApp
+//
+//  Created by vipul kumar on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InventoryManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
